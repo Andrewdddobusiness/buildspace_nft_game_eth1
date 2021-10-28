412582d7887941cb4e039efa744e51829627b029
Andrewdddobusiness <andrewdddobusiness@gmail.com>
2021-10-28 14:48:05 +1100

    update commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x255389A561BDe35C9973216e83ed8720599B6115';
+
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+
+export { CONTRACT_ADDRESS, transformCharacterData };
